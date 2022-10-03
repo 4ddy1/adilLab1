a738412ef7d8055a7702913531612b89a3647d8f
addy <adil_15-05@mail.ru>
2022-10-04 04:37:13 +0600

    1 lab for Altman E.A.

---
+++ src/main/kotlin/Client.kt
@@ -0,0 +1,59 @@
+
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.html.*
+import kotlinx.html.dom.append
+import kotlinx.html.js.div
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.Node
+import org.w3c.dom.events.Event
+
+fun main() {
+  window.onload = { document.body?.sayHello() }
+}
+
+fun Node.sayHello() {
+  append {
+      div {
+        input {
+          id = "cel"
+          placeholder = "enter cel"
+          type = InputType.number
+        }
+      }
+      div {
+        input {
+          id = "far"
+          placeholder = "enter far"
+          type = InputType.number
+        }
+      }
+      form {
+        button {
+          +"cel To Far"
+          onClickFunction = ::celToFar
+        }
+        button {
+          +"far To Cel"
+          onClickFunction = ::farToCel
+        }
+      }
+  }
+}
+
+
+
+fun celToFar(e: Event): Unit {
+  var cel = document.getElementById("cel") as HTMLInputElement
+  var far = document.getElementById("far") as HTMLInputElement
+
+  far.value = ((9.0 / 5.0 * cel.value.toDouble()) + 32).toString()
+}
+
+fun farToCel(e: Event): Unit {
+  var cel = document.getElementById("cel") as HTMLInputElement
+  var far = document.getElementById("far") as HTMLInputElement
+
+  cel.value = (5.0 / 9.0 * (far.value.toDouble() - 32)).toString()
+}
